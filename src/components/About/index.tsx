4959ad215192bf8940c6b2af955216cb57a2f028
Vinícius Lima <viniciuslimaandre@hotmail.com>
2023-10-31 19:02:57 -0300

    feat: add target blank to social links

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -56,31 +56,31 @@ const About = ({ data }: Props) => {
 
             <Social>
               {data.whatsapp && (
-                <a href={data.whatsapp}>
+                <a href={data.whatsapp} target="_blank" rel="noreferrer">
                   <Icon icon="ph:whatsapp-logo-duotone" />
                 </a>
               )}
 
               {data.facebook && (
-                <a href={data.facebook}>
+                <a href={data.facebook} target="_blank" rel="noreferrer">
                   <Icon icon="ph:facebook-logo-duotone" />
                 </a>
               )}
 
               {data.instagram && (
-                <a href={data.instagram}>
+                <a href={data.instagram} target="_blank" rel="noreferrer">
                   <Icon icon="ph:instagram-logo-duotone" />
                 </a>
               )}
 
               {data.linkedin && (
-                <a href={data.linkedin}>
+                <a href={data.linkedin} target="_blank" rel="noreferrer">
                   <Icon icon="ph:linkedin-logo-duotone" />
                 </a>
               )}
 
               {data.github && (
-                <a href={data.github}>
+                <a href={data.github} target="_blank" rel="noreferrer">
                   <Icon icon="ph:github-logo-duotone" />
                 </a>
               )}
