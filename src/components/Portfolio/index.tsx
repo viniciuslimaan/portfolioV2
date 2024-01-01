069a6d3ed38bd15929225dd70122cdd30367b290
Vinícius Lima <viniciuslimaandre@hotmail.com>
2024-01-01 17:23:39 -0300

    refactor: fix writing error in portfolio button

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -198,7 +198,7 @@ const Portfolio = () => {
             target="_blank"
             rel="noreferrer"
           >
-            porfólio
+            portfólio
           </a>
           .
         </LinkGit>
